623610c66143141a7a0e31cfdfbf78664bb38367
Dmitriy Kasimov <its.d,itriy.kasimov@gmail.com>
2025-03-30 15:42:41 +0300

    поэкспериментировал с деревом

--- src/widgets/VansTree/const/tree.ts
+++ src/widgets/VansTree/const/tree.ts
@@ -4,24 +4,40 @@ import { links } from './links.ts';
 export function tree() {
     // set your license key here before creating the diagram: go.Diagram.licenseKey = "...";
     const diagram = new go.Diagram({
-        'undoManager.isEnabled': true,
-
         layout: new go.TreeLayout({ angle: 0, layerSpacing: 100 }),
     });
 
+    diagram.linkTemplate = new go.Link().add(
+        new go.Shape({ stroke: 'white' }), // the link shape
+        new go.Shape({ toArrow: 'Standard', stroke: 'white' }), // the arrowhead
+    );
+
     // define a simple Node template
     diagram.nodeTemplate = new go.Node('Vertical', {
-        background: 'white',
+        margin: 10,
+        background: 'black',
     }).add(
+        new go.TextBlock('Default Text', {
+            stroke: 'white',
+            font: 'bold 16px sans-serif',
+            textAlign: 'left',
+        }).bind('text', 'name'),
         new go.Picture({
             width: 120,
             height: 60,
         }).bind('source'),
-        new go.TextBlock('Default Text', {
-            margin: 12,
-            stroke: 'black',
-            font: 'bold 16px sans-serif',
-        }).bind('text', 'name'),
+
+        go.GraphObject.build('Button', {
+            margin: 2,
+            width: 120,
+            'ButtonBorder.fill': '#5CD039',
+            _buttonFillOver: '#5FB744',
+        }).add(
+            new go.TextBlock('Исследовать', {
+                font: 'bold 14px sans-serif',
+                stroke: 'white',
+            }),
+        ),
     );
 
     // the same model as before
