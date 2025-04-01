07209414d53740c13d12307173f678356c382b32
Dmitriy Kasimov <its.d,itriy.kasimov@gmail.com>
2025-04-01 21:02:35 +0300

    remove unused; добавил поддержку стрелок для дерева грузовиков

--- src/widgets/TestWidget/ui/TestWidget.tsx
+++ src/widgets/TestWidget/ui/TestWidget.tsx
@@ -1,8 +1,6 @@
 import { FC } from 'react';
-import Xarrow, { Xwrapper } from 'react-xarrows';
-import { TruckCard } from '@/entities/TruckCard';
 import { TruckModels } from '@/shared/const/TruckModels.ts';
-import { Card, HStack, VStack } from '@project-1114/ui-kit';
+import { Card } from '@project-1114/ui-kit';
 
 import cls from './TestWidget.module.scss';
 import { TreeTrucks } from '@/widgets/TreeTrucks';
@@ -55,73 +53,6 @@ export const TestWidget: FC = () => {
     return (
         <Card className={cls.Card}>
             <TreeTrucks trees={trees} />
-            <Xwrapper>
-                <HStack gap={'xl'}>
-                    <TruckCard
-                        id={'elem1'}
-                        model={TruckModels.Frightliner}
-                        state={{
-                            explored: true,
-                            bought: true,
-                            canSale: true,
-                            priceCashForSale: 100500,
-                        }}
-                    />
-                    <VStack gap={'xl'}>
-                        <TruckCard
-                            id={'elem2'}
-                            model={TruckModels.Hauler}
-                            state={{
-                                explored: true,
-                                bought: false,
-                                priceCash: 1000,
-                                needCash: 0,
-                            }}
-                        />
-                        <HStack gap={'xl'}>
-                            <TruckCard
-                                id={'elem3'}
-                                model={TruckModels.Phantom}
-                                state={{
-                                    explored: true,
-                                    bought: false,
-                                    priceCash: 1010,
-                                    needCash: 0,
-                                }}
-                            />
-                            <TruckCard
-                                id={'elem4'}
-                                model={TruckModels.Phantom2}
-                                state={{
-                                    explored: false,
-                                    bought: false,
-                                    priceScore: 100,
-                                    needScore: 0,
-                                }}
-                            />
-                        </HStack>
-                    </VStack>
-
-                    <Xarrow
-                        start={'elem1'} //can be react ref
-                        end="elem2" //or an id
-                        color={'white'}
-                        strokeWidth={2}
-                    />
-                    <Xarrow
-                        start={'elem1'} //can be react ref
-                        end="elem3" //or an id
-                        color={'white'}
-                        strokeWidth={2}
-                    />
-                    <Xarrow
-                        start={'elem3'} //can be react ref
-                        end="elem4" //or an id
-                        color={'white'}
-                        strokeWidth={2}
-                    />
-                </HStack>
-            </Xwrapper>
         </Card>
     );
 };
--- src/widgets/TreeTrucks/helpers/getTrucksTree.tsx
+++ src/widgets/TreeTrucks/helpers/getTrucksTree.tsx
@@ -2,6 +2,7 @@ import { TTrucksTree } from '../model/types/TTrucksTree.ts';
 import { TruckInfo } from '@/shared/const/TruckInfo.ts';
 import { TruckCard } from '@/entities/TruckCard';
 import { HStack, VStack } from '@project-1114/ui-kit';
+import Xarrow from 'react-xarrows';
 
 export const getTrucksTree = (trees: TTrucksTree[]) => {
     return trees.map((tree) => (
@@ -12,7 +13,17 @@ export const getTrucksTree = (trees: TTrucksTree[]) => {
                 state={tree.state}
             />
             {tree.children ? (
-                <VStack gap={'l'}>{getTrucksTree(tree.children)}</VStack>
+                <VStack gap={'l'}>
+                    {getTrucksTree(tree.children)}
+                    {tree.children.map((childTree) => (
+                        <Xarrow
+                            start={TruckInfo[tree.model].name} //can be react ref
+                            end={TruckInfo[childTree.model].name} //or an id
+                            color={'white'}
+                            strokeWidth={2}
+                        />
+                    ))}
+                </VStack>
             ) : null}
         </HStack>
     ));
